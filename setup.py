06c7d40a648bc33bda5fc24b105a4144c5f3be67
syed saleem <junaidfaizan024@gmail.com>
2024-11-20 11:55:43 -0800

    medical chatbot

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'syed saleem',
+    author_email= 'junaidfaizan024@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
